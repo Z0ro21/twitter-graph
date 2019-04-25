1e42a25b1419baa4a9fa9c8165d749dcf0c8d1c6
Adrian Paredes <luis.paredes@dsindigo.com>
2019-04-25 17:10:01 -0500

    Agrega conexion con cloud Function

--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFireFunctions } from '@angular/fire/functions';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TwitterService {
 
-  constructor(private fns: AngularFireFunctions) { 
+  constructor(public http: HttpClient) {
+  }
+
+  getCommonFriends(userNames: string[]): Observable<string[]> {
+    return this.http.post<string[]>('https://us-central1-twitter-graph-codecamp.cloudfunctions.net/usersFollowers', userNames, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
   }
 }
